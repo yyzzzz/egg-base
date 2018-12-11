b463ae22ea503bf0e1f9ebdccdac8e04d10ebd86
yyzzzz <819053597@qq.com>
2018-12-11 23:49:59 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -1,4 +1,5 @@
 'use strict';
+const Sequelize = require('sequelize');
 
 module.exports = appInfo => {
   const config = exports = {};
@@ -8,6 +9,32 @@ module.exports = appInfo => {
 
   // add your config here
   config.middleware = [];
+
+  config.sequelize = {
+    dialect: 'mysql', // support: mysql, mariadb, postgres, mssql
+    database: 'eggjs-example',
+    host: '127.0.0.1',
+    port: 3306,
+    username: "root",
+    password: "1230",
+    operatorsAliases: false,
+    timezone: '+08:00'
+  };
+
+  // 关闭csrf
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    // 白名单
+    // domainWhiteList: ['http://localhost:8080'],
+  }
+
+  // 允许规则
+  config.cors = {
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
+  };
 
   return config;
 };
---
+++ database/migrations/20181211135236-init-users.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   const { INTEGER, DATE, STRING } = Sequelize;
+    await queryInterface.createTable('user', {
+      id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+      name: STRING(30),
+      age: INTEGER,
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+   await queryInterface.dropTable('user');
+
+  }
+};
